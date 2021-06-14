e0b690ad4e0a0c2bcbb16d84a514c0d3c638de43
NonzwakaziMgxaji <nonzwakazimgxaji@gmail.comgit config --listgit config --global user.email nonzwakazimgxaji@gmail.com>
2021-06-14 12:08:49 +0200

    Fixed Sunday which was not picking up any colour change

--- matchingDays.js
+++ matchingDays.js
@@ -1,4 +1,3 @@
-document.addEventListener('DOMContentLoaded', function () {
     // get a reference to the template script tag
     var template = document.querySelector(".matchingDaysTemplate").innerHTML;
 
@@ -21,14 +20,16 @@ document.addEventListener('DOMContentLoaded', function () {
 
     //displays the days of the week on screen using the compiled template
     weekDayDisplay.innerHTML = weekDayTemplate({ weekDays: matchDaysInstance.weekDaysArray() });
+    
     var days = weekDayDisplay.children;
 
     //change event listener for the first date change
-    datesElem.addEventListener("change", function () {
+    datesElem.addEventListener("change", function () {  
         var dateSelected1 = new Date(eersteDatum.value);
         var daySelected1 = dateSelected1.getDay();
         var dateSelected2 = new Date(tweedeDatum.value);
         var daySelected2 = dateSelected2.getDay();
+    
 
         for (var i = 0; i < days.length; i++) {
             var day = days[i];
@@ -40,12 +41,11 @@ document.addEventListener('DOMContentLoaded', function () {
         if (daySelected1 === daySelected2) {
             days[daySelected2].classList.add("matchingColour");
         } else if (daySelected1 != daySelected2) {
-            if (daySelected1) {
+            if (dateSelected1) {
                 days[daySelected1].classList.add("diffColour1");
             }
-            if (daySelected2) {
+            if (dateSelected2) {
                 days[daySelected2].classList.add("diffColour2");
             }
         }
     });
-})
